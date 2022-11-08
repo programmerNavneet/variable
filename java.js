0f824be8e9b8a67587312612b9ce51d68793629f
programmerNavneet <117737158+programmerNavneet@users.noreply.github.com>
2022-11-08 18:09:47 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+var age=34;     //var variable
+age=54;
+var name = "John";
+var developer = true;
+console.log(age)
+console.log(name);
+console.log(developer);
+
+const detail="Navneet";       //const variable
+detail ="sharma";
+console.log(detail);
+
+// let number=45;
+// number=12;
+// console.log(number);
+
+// {
+//     let number=45;
+//     number=12;
+//     console.log(number)
+// }
